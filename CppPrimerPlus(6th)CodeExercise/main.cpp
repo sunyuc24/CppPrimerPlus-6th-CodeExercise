6910a830bfb706db1d4d682170425e250c5e224e
sunyuc24 <690510390@qq.com>
2019-08-03 23:20:57 +0800

    add the existing files to local reporitory

---
+++ CppPrimerPlus(6th)CodeExercise/main.cpp
@@ -0,0 +1,33 @@
+#include "Ch2.h"
+#include "Ch3.h"
+int main()
+{
+	//Ch2 ch2;
+	//ch2.Ch2_1();
+	//ch2.Ch2_2();
+	//ch2.Ch2_4();
+	//std::cout << "Please enter a Celsius value: ";
+	//double degree;
+	//std::cin >> degree;
+	//ch2.Ch2_5(degree);
+	//std::cout << " Enter the number of light years: \n";
+	//double light_years;
+	//std::cin >> light_years;
+	//ch2.Ch2_6(light_years);
+	//int hours, mins;
+	//std::cout << "Enter the number of hours: ";
+	//std::cin >> hours;
+	//std::cout << "Enter the number of mins: ";
+	//std::cin >> mins;
+	//ch2.Ch2_7(hours, mins);
+
+	Ch3 ch3;
+	//ch3.Ch3_1();
+	//ch3.Ch3_2();
+	//ch3.Ch3_3();
+	//ch3.Ch3_4();
+	//ch3.Ch3_5();
+	ch3.Ch3_6();
+	system("pause");
+
+}
