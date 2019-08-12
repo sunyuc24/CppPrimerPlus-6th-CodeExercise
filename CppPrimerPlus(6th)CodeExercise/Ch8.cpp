ba76110f0643902bed4d4112e14b746ad752c0a4
sunyuc24 <690510390@qq.com>
2019-08-13 01:09:25 +0800

    Update Ch8

--- CppPrimerPlus(6th)CodeExercise/Ch8.cpp
+++ CppPrimerPlus(6th)CodeExercise/Ch8.cpp
@@ -43,6 +43,94 @@ void Ch8::Ch8_2(){
 	Ch8_2showCandyBar(candybar);
 }
 
-Ch8::CandyBar & Ch8::Ch8_2setCandyBar(CandyBar & candybar, char * str, float fweight, int ncaloire){
-	
+Ch8::CandyBar & Ch8::Ch8_2setCandyBar(CandyBar& candybar, char * str, float fweight, int ncalorie){
+	candybar.sbrand = str;
+	candybar.fweight = fweight;
+	candybar.ncalorie = ncalorie;
+	return candybar;
+}
+
+void Ch8::Ch8_2showCandyBar(const CandyBar& candybar){
+	cout << "CandyBar's brand name: " << candybar.sbrand << endl
+		<< "CandyBar's weight: " << candybar.fweight << endl
+		<< "CandyBar's calorie: " << candybar.ncalorie << endl;
+}
+
+void Ch8::Ch8_3(){
+	string str;
+	cout << "Enter a string (q to quit): ";
+	while (getline(cin, str) )
+	{
+		if (str[0] == 'q' && str.size() == 1){
+			break;
+		}
+		Ch8_3toupper(str);
+		cout << "Next string (q to quit): ";
+		
+	}
+}
+
+void Ch8::Ch8_3toupper(const string& str){
+	char ch;
+	for (int i = 0; i < str.size(); i++){
+		ch = str[i];
+		if (isalpha(ch))
+		{
+			cout << (char)toupper(ch);
+		}
+		else
+		{
+			cout << str[i];
+		}
+	}
+	cout << endl;
+}
+
+void Ch8::Ch8_4(){
+	stringy beany;
+	char testing[] = "Reality isn't what it used to be.";
+	Ch8_4set(beany, testing);
+	Ch8_4show(beany);
+	Ch8_4show(beany, 2);
+	testing[0] = 'D';
+	testing[1] = 'U';
+	Ch8_4show(testing);
+	Ch8_4show(testing, 3);
+	Ch8_4show("Done!");
+}
+
+void Ch8::Ch8_4set(stringy& stct, char ch[]){
+	char *pch = new char[sizeof(ch)];
+	for (int i = 0;i < sizeof(ch); i++)
+	{
+		pch[i] = ch[i];
+	}
+	stct.str = pch;
+	stct.ct = sizeof(ch)-1;
+}
+
+void Ch8::Ch8_4show(const stringy& stct){
+	cout << stct.str;
+}
+
+void Ch8::Ch8_4show(const stringy& stct, int n /* = 1 */){
+	for (int i = 0; i < n; i++){
+		cout << stct.str << endl;
+	}
+}
+
+void Ch8::Ch8_4show(const char ch[]){
+	for (int i = 0; i < sizeof(ch); i++){
+		cout << ch[i];
+	}
+	cout << endl;
+}
+
+void Ch8::Ch8_4show(const char ch[], int n /* = 1 */){
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < sizeof(ch); j++){
+			cout << ch[i];
+		}
+		cout << endl;
+	}
 }
--- CppPrimerPlus(6th)CodeExercise/Ch8.h
+++ CppPrimerPlus(6th)CodeExercise/Ch8.h
@@ -7,16 +7,21 @@
 #include <string>
 #include <cstring>
 #include <cctype>
-
+using std::string;
 class Ch8
 {
 private:
 	struct CandyBar
 	{
-		string sbrand;
+		std::string sbrand;
 		float fweight;
 		int ncalorie;
 	};
+	struct stringy{
+		char* str;
+		int ct;
+	};
+
 public:
 	Ch8();
 	~Ch8();
@@ -24,7 +29,15 @@ public:
 	void Ch8_1str(const char *, int n = 0);
 	void Ch8_2();
 	CandyBar & Ch8_2setCandyBar(CandyBar &, char * str = "Millennium Munch", float fweight = 2.85, int ncaloire = 350);
-	void Ch8_2showCandyBar(CandyBar &);
+	void Ch8_2showCandyBar(const CandyBar &);
+	void Ch8_3();
+	void Ch8_3toupper(const string&);
+	void Ch8_4();
+	void Ch8_4set(stringy&, char *);
+	void Ch8_4show(const stringy&);
+	void Ch8_4show(const stringy&, int n = 1);
+	void Ch8_4show(const char ch[]);
+	void Ch8_4show(const char ch[], int n = 1);
 };
 
 #endif
